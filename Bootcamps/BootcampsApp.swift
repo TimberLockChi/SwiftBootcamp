16fae52b4cc2127fc18b5472b1c666df30d3af9d
chitim <timchi0305@163.com>
2023-07-31 15:23:25 +0800

    Initial Commit

---
+++ Bootcamps/BootcampsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BootcampsApp.swift
+//  Bootcamps
+//
+//  Created by Chi Tim on 2023/7/31.
+//
+
+import SwiftUI
+
+@main
+struct BootcampsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
